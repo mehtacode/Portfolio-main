8ea0c98675b89ba0e3282217b4a7a82674e6498d
Aditya Mehta <maxedge360@gmail.com>
2024-12-25 00:04:10 +0530

    new Updates

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const toggleBtn = document.getElementById('toggle-btn');
+    const navLinks = document.getElementById('nav-links');
+
+    toggleBtn.addEventListener('click', () => {
+      navLinks.classList.toggle('active');
+    });
 
